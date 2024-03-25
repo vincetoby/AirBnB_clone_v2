a42d09a05ec13bbba6cf92793c7539382f15897b
Vincent Ekechukwu <136026786+vincetoby@users.noreply.github.com>
2024-03-25 01:00:22 +0100

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
+"""this starts a Flask web application.
+which listens on 0.0.0.0, port 5000.
 Routes:
     /cities_by_states: HTML page with a list of all states and related cities.
 """
@@ -14,9 +13,8 @@ app = Flask(__name__)
 
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
-    """Displays an HTML page with a list of all states and related cities.
-
-    States/cities are sorted by name.
+    """this displays a HTML page with a list of all states and related cities.
+    the States/cities are sorted by name.
     """
     states = storage.all("State")
     return render_template("8-cities_by_states.html", states=states)
@@ -24,7 +22,7 @@ def cities_by_states():
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+    """this removes the current SQLAlchemy session."""
     storage.close()
 
 
